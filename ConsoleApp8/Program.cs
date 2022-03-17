2ad5cfb25f1b1e3e35a9fcb31297d957e580b42d
Michał Sałek <michal.salek@microsoft.wsei.edu.pl>
2022-03-17 20:46:20 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ulamek a = new Ulamek(4, 9);
+            Ulamek b = new Ulamek(21, 3);
+            Ulamek c = new Ulamek(a);
+            new Ulamek();
+
+            List<Ulamek> ulamki = new List<Ulamek>
+            {
+                new Ulamek(4, 9), new Ulamek(21, 3),
+                new Ulamek(),
+            };
+            ulamki.Sort();
+            ulamki.ForEach(ulamek => Console.WriteLine(ulamek.Numerator + "/" + ulamek.Denominator));
+
+            Console.WriteLine(a / b);
+
+            Console.WriteLine(a.Equals(b));
+
+            Console.WriteLine(a.RoundDown());
+            Console.WriteLine(a.RoundUp());
+        }
+    }
+}
---
+++ ConsoleApp8/Ulamek.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    public class Ulamek : IEquatable<Ulamek>, IComparable<Ulamek>
+    {
+
+        // zmienne
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+
+
+        // konstruktor domyslny
+        public Ulamek()
+        {
+            Numerator = 5;
+            Denominator = 5;
+        }
+
+        // konstruktor
+        public Ulamek(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        // konstruktor kopiujacy
+        public Ulamek(Ulamek previusUlamek)
+        {
+            Numerator = previusUlamek.Numerator;
+            Denominator = previusUlamek.Denominator;
+        }
+
+        // konwertowanie obiektu na string
+        public override string ToString()
+        {
+            return Numerator.ToString() + "/" + Denominator.ToString();
+        }
+
+        // obliczanie najwiekszego wspolnego dzielnika
+        private static int GCD(int a, int b)
+        {
+            int x;
+            while (b != 0)
+            {
+                x = b;
+                b = a % b;
+                a = x;
+            }
+            return a;
+        }
+
+        // metoda zaokraglenie w dol
+        public int RoundDown()
+        {
+            double digitDown = (double)Numerator / (double)Denominator;
+            return (int)Math.Floor(digitDown);
+        }
+        
+        // metoda zaokraglenie w gore
+        public int RoundUp()
+        {
+            double digitUp = (double)Numerator / (double)Denominator;
+            return (int)Math.Ceiling(digitUp);
+        }
+
+        // interfejs sprawdzanie czy rowne
+        public bool Equals([AllowNull] Ulamek other)
+        {
+            if( Numerator / Denominator == other.Numerator / Denominator)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // interfejs porownania
+        public int CompareTo([AllowNull] Ulamek other)
+        {
+            return Numerator / Denominator.CompareTo(other.Numerator / Denominator);
+        }
+
+        // operatory dodawania, odejmowania, mnozenia, dzielenia + - * / 
+        public static Ulamek operator +(Ulamek ulamek1, Ulamek ulamek2)
+        {
+            if (ulamek1.Denominator == ulamek2.Denominator) {
+
+                return new Ulamek(ulamek1.Numerator + ulamek2.Numerator / GCD(ulamek1.Numerator + ulamek2.Numerator, ulamek1.Denominator), ulamek1.Denominator / GCD(ulamek1.Numerator + ulamek2.Numerator, ulamek1.Denominator));
+            }
+            else
+            {
+                int numerator = ulamek1.Numerator * ulamek2.Denominator + ulamek1.Denominator * ulamek2.Numerator;
+                int denominator = ulamek1.Denominator * ulamek2.Denominator;
+                return new Ulamek(numerator / GCD(numerator, denominator), denominator / GCD(numerator, denominator));
+            }
+        }
+        public static Ulamek operator -(Ulamek ulamek1, Ulamek ulamek2)
+        {
+            if (ulamek1.Denominator == ulamek2.Denominator)
+            {
+                return new Ulamek(ulamek1.Numerator - ulamek2.Numerator / GCD(ulamek1.Numerator - ulamek2.Numerator, ulamek1.Denominator), ulamek1.Denominator / GCD(ulamek1.Numerator - ulamek2.Numerator, ulamek1.Denominator));
+            }
+            else
+            {
+                int numerator = ulamek1.Numerator * ulamek2.Denominator - ulamek1.Denominator * ulamek2.Numerator;
+                int denominator = ulamek1.Denominator * ulamek2.Denominator;
+                return new Ulamek(numerator / GCD(numerator, denominator), denominator / GCD(numerator, denominator));
+            }
+        }
+        public static Ulamek operator *(Ulamek ulamek1, Ulamek ulamek2)
+        {
+            int numerator, denominator;
+
+            numerator = ulamek1.Numerator * ulamek2.Numerator;
+            denominator = ulamek1.Denominator* ulamek2.Denominator;
+
+            return new Ulamek(numerator / GCD(numerator, denominator), denominator / GCD(numerator, denominator) );
+        }
+        public static Ulamek operator /(Ulamek ulamek1, Ulamek ulamek2)
+        {
+            int numerator, denominator;
+
+            numerator = ulamek1.Numerator * ulamek2.Denominator;
+            denominator = ulamek1.Denominator * ulamek2.Numerator;
+
+            return new Ulamek(numerator / GCD(numerator, denominator), denominator / GCD(numerator, denominator));
+        }
+
+    }
+}
